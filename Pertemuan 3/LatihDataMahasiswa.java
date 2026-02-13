d00a2778ecc4a45ce5695aee62363ef9b611a8cf
KristianCandraDinata <d1041241033@student.untan.ac.id>
2026-02-13 16:28:18 +0700

    Pertemuan 3

---
+++ Pertemuan 3/LatihDataMahasiswa.java
@@ -0,0 +1,22 @@
+public class LatihDataMahasiswa {
+    public static void main(String[] args){
+
+        System.out.println("=== DATA MAHASISWA ===");
+        
+        long nim = 2301010001L;
+        String nama = "Budi Santoso";
+        char kelas = 'A';
+        boolean statusAktif = true;
+        double ipk = 3.75;
+        int sks = 72;
+
+        //Menampilkan nilai semua variabel
+        System.out.println("NIM             : " + nim);
+        System.out.println("Nama            : " + nama);
+        System.out.println("Kelas           : " + kelas);
+        System.out.println("Status Aktif    : " + statusAktif);
+        System.out.println("IPK Semester    : " + ipk);
+        System.out.println("Total SKS       : " + sks);
+         
+    }
+}
---
+++ Pertemuan 3/LatihPerhitungan.java
@@ -0,0 +1,16 @@
+public class LatihPerhitungan {
+    public static void main(String[] args) {
+
+        System.out.println("=== SIMULASI TRANSAKSI BANK ===");
+        int saldo = 1000000;
+        System.out.println("Saldo Awal           : " + saldo);
+        int setoran = 500000;
+        System.out.println("Setoran              : " + setoran);
+        saldo += setoran;
+        System.out.println("Saldo Setelah Setor  : " + saldo);
+        int penarikan = 250000;
+        System.out.println("Penarikan            : " + penarikan);
+        saldo -= penarikan;
+        System.out.println("Saldo Akhir          : " + saldo);
+    }
+}
